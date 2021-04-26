6217144a16004d2c1186ae6def4c8f7e20554d6b
yigal irani <yigal@symbolclick.com>
2021-04-26 07:03:02 -0400

    Update app.js
    deleted old comment, empty lines

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import {Label} from './widgets.js'
 
-
-function App(){ //probably should consolidate some of these state variables
+function App(){
   return <Label>hello react</Label>
 }
 ReactDOM.render(<App />,document.querySelector('#root') );
